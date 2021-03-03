977c07ca4d43cdbe13f40b870bcdc81ef0a4c7b3
ee2405 <Melody90112>
2021-03-02 21:18:59 -0800

    add hw1.py

---
+++ hw1.py
@@ -0,0 +1,30 @@
+import csv
+
+cwb_filename = '108061101.csv'
+data = []
+target_data = []
+for i in range(5): 
+    target_data.append([])
+station_list = ['C0A880', 'C0F9A0', 'C0G640', 'C0R190', 'C0X260']
+answer = []
+
+with open(cwb_filename) as csvfile:
+   mycsv = csv.DictReader(csvfile)
+   for row in mycsv:
+      data.append(row)
+
+for station in station_list:
+    for row in data:
+        if row['station_id'] == station and row['PRES'] != '-99.000' and row['PRES'] != '-999.000':
+            target_data[station_list.index(station)].append(float(row['PRES']))
+for i in range(5):
+    mean = 0
+    for j in range(len(target_data[i])):
+            mean += target_data[i][j]
+    mean = mean / len(target_data[i])
+    if mean == 0:
+        answer.append([station_list[i], 'None'])
+    else:
+        answer.append([station_list[i], mean])
+
+print(answer)
